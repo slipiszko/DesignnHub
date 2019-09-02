a1301f490e6ba8e9da691c9cff5dffbb39195f9d
Sebastian Lipiszko <lipsikko@gmail.com>
2019-09-02 14:49:30 +1000

    Removed unneccasry devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :profiles, only: [:edit, :update]
+
+  devise_for :users, skip: :registrations
+  as :user do
+    get    '/users/cancel',    to: 'devise/registrations#cancel', as: 'cancel_user_registration'
+    post   '/users',           to: 'devise/registrations#create', as: 'user_registration'
+    delete '/users',           to: 'devise/registrations#destroy'
+    get    '/users/sign_up',   to: 'devise/registrations#new', as: 'new_user_registration'
+    # get    '/users/edit'      to: 'devise/registrations#edit', as: 'edit_user_registration'
+    # patch  '/users',           to: 'devise/registrations#update'
+    # put    '/users',           to: 'devise/registrations#update'
+  end
 end
