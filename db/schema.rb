88afe602b577109181869eeb0aa4845b122cee10
Sebastian Lipiszko <lipsikko@gmail.com>
2019-09-02 15:31:07 +1000

    Migrated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_02_021040) do
+ActiveRecord::Schema.define(version: 2019_09_02_023746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "designs", force: :cascade do |t|
+    t.string "photo"
+    t.string "title"
+    t.string "description"
+    t.string "category"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_designs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,4 +45,5 @@ ActiveRecord::Schema.define(version: 2019_09_02_021040) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "designs", "users"
 end
