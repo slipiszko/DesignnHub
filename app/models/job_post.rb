17a9c78a59480f308652f131c0891cf7977b4051
Sebastian Lipiszko <lipsikko@gmail.com>
2019-09-05 14:59:15 +1000

    Added validations to JobPost model

--- app/models/job_post.rb
+++ app/models/job_post.rb
@@ -1,3 +1,8 @@
 class JobPost < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+  validates :city, presence: true
 end
