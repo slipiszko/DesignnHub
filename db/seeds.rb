ae4274d3d9137f621aa347ab9d96e2817275026c
Sebastian Lipiszko <lipsikko@gmail.com>
2019-09-05 16:24:14 +1000

    Designs in seed file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,41 +33,117 @@ puts "Generating 13 Designs..."
 
 designs = []
 
-design_images = [
-  'app/assets/images/design_test_1.png',
-  'app/assets/images/design_test_2.png',
-  'app/assets/images/design_test_3.png',
-  'app/assets/images/design_test_4.png',
-  'app/assets/images/design_test_5.png',
-  'app/assets/images/design_test_6.png'
-]
-
-13.times { designs <<
-  design = Design.create!(
-  photo: open(design_images.sample),
-  title: "Design title",
-  description: "Design description",
-  category: "photography",
+designs << Design.create!(
+  photo: open('app/assets/images/design_test_2.jpg'),
+  title: "Crisp (Customer Messaging App) Saas website",
+  description: "I had the pleasure to help the guys are Crisp bring their brand to a place worthy of their culture and ambitions.",
+  category: "Web desgin",
   user: users.sample
-  )
-}
+)
+
+designs << Design.create!(
+  photo: open('app/assets/images/design_test_4.jpg'),
+  title: "Windmill town",
+  description: "Is there such a place, the blue sky, the gloden earth, the wind blwoing the grass, everything is so beautiful.",
+  category: "Illistration",
+  user: users.sample
+)
+
+designs << Design.create!(
+  photo: open('app/assets/images/design_test_3.jpg'),
+  title: "Fashion Web UI",
+  description: "Fashion webite design.",
+  category: "Web desgin",
+  user: users.sample
+)
+
+designs << Design.create!(
+  photo: open('app/assets/images/design_test_7.jpg'),
+  title: "Put the Yee in Haw",
+  description: "Monday morning type warm-up inspired by the weekend. Have more weekends that keep you sayin' YEE-FUCKING-HAW!",
+  category: "Typography",
+  user: users.sample
+)
+
+designs << Design.create!(
+  photo: open('app/assets/images/design_test_5.jpg'),
+  title: "Sunset hike",
+  description: "Personal work / revamp / repost",
+  category: "Illistration",
+  user: users.sample
+)
+
+designs << Design.create!(
+  photo: open('app/assets/images/design_test_1.jpg'),
+  title: "Food web page mockup",
+  description: "Made this is in my freetime between jobs",
+  category: "Web desgin",
+  user: users.sample
+)
+
+designs << Design.create!(
+  photo: open('app/assets/images/design_test_6.jpg'),
+  title: "Room to Roam Pennant",
+  description: "We love how our Room to Roam Pennant came out! Manufactured",
+  category: "Typography",
+  user: users.sample
+)
+
+designs << Design.create!(
+  photo: open('app/assets/images/design_test_8.jpg'),
+  title: "Passion fruit",
+  description: "After moving to Taiwan I discovered there were so many various fruits and started to draw some of them. For this project I used my Ipad Pro and my Apple pencil. Yummny!",
+  category: "Typography",
+  user: users.sample
+)
+
+designs << Design.create!(
+  photo: open('app/assets/images/design_test_11.jpg'),
+  title: "Cafe Tropical",
+  description: "Just for fun exploration for this damn funny show.",
+  category: "Illistration",
+  user: users.sample
+)
+
+designs << Design.create!(
+  photo: open('app/assets/images/design_test_9.jpg'),
+  title: "VR Tennis Game App",
+  description: "Connect your app to VR glasses, to play with those legendary tennis starts. Select the courts you prefer and choose the different levels",
+  category: "Web desgin",
+  user: users.sample
+)
+
+designs << Design.create!(
+  photo: open('app/assets/images/design_test_10.jpg'),
+  title: "Polar Bear Holding Coca Cola and Flowers",
+  description: "Just for fun",
+  category: "Illistration",
+  user: users.sample
+)
 
 puts "Finished!"
 
 puts "Generating Design Tags..."
 
 tags = [
-  "Photography",
-  "Architecture",
-  "Web design",
-  "Paint",
-  "Typography",
-  "Product Design"
+  "Adobe",
+  "Branc",
+  "Photoshop",
+  "Cartoon",
+  "Illistration",
+  "Logo",
+  "Illustrator",
+  "Animation",
+  "Calligraphy",
+  "Web",
+  "Web Design",
+  "UX",
+  "UI",
+  "After Effects"
 ]
 
 tags.each do |tag|
   DesignTag.create!(
-    design: designs.sample,
     name: tag
   )
 end
@@ -86,8 +162,8 @@ content = [
 designs.uniq.each do |design|
   5.times { comment = Comment.create!(
     content: content.sample,
-    longitude:'0.231',
-    latitude:'0.1738',
+    x: '0.231',
+    y: '0.1738',
     design: design,
     user: users.sample
     )
