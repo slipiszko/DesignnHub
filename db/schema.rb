7f3e12e75e250c05c9d5024984149a04ca836063
Sebastian Lipiszko <lipsikko@gmail.com>
2019-09-05 14:02:50 +1000

    No changes made

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_233912) do
+ActiveRecord::Schema.define(version: 2019_09_05_034538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,18 @@ ActiveRecord::Schema.define(version: 2019_09_04_233912) do
     t.index ["user_id"], name: "index_designs_on_user_id"
   end
 
+  create_table "job_posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "photo"
+    t.string "category"
+    t.string "city"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_job_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -81,6 +93,7 @@ ActiveRecord::Schema.define(version: 2019_09_04_233912) do
   add_foreign_key "comments", "users"
   add_foreign_key "design_tags", "designs"
   add_foreign_key "designs", "users"
+  add_foreign_key "job_posts", "users"
   add_foreign_key "votes", "comments"
   add_foreign_key "votes", "users"
 end
