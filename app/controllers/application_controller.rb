5be3f32f0d9892dead311f048daa6fbdb64f7f25
Sebastian Lipiszko <lipsikko@gmail.com>
2019-09-06 10:24:39 +1000

    JobPost CRUD added to job_posts_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  before_action :set_design
+  before_action :set_new_design
   def set_design
     @new_design = Design.new
   end
