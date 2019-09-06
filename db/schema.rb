b26a87767365e2ee68f2a8cf239a65eac27fcce2
Sebastian Lipiszko <lipsikko@gmail.com>
2019-09-06 12:10:56 +1000

    Design.description changed to text type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_05_074821) do
+ActiveRecord::Schema.define(version: 2019_09_06_020926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2019_09_05_074821) do
   create_table "designs", force: :cascade do |t|
     t.string "photo"
     t.string "title"
-    t.string "description"
+    t.text "description"
     t.string "category"
     t.bigint "user_id"
     t.datetime "created_at", null: false
