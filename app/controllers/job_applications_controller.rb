a93af8cbd5e931cc72c0ad3ebc4beeeacfa56dd9
Sebastian Lipiszko <lipsikko@gmail.com>
2019-09-06 16:16:26 +1000

    New & create job application methods implemented

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -1,5 +1,23 @@
 class JobApplicationsController < ApplicationController
+  before_action :find_job_post, only: [:new, :create]
   def new
+    @job_application = JobApplication.new
+  end
 
+  def create
+    @job_application = JobApplication.new(job_application_params)
+    @job_application.job_post = @job_post
+    @job_application.user = current_user
+    @job_application.save
+  end
+
+  private
+
+  def find_job_post
+    @job_post = JobPost.find(params[:job_post_id])
+  end
+
+  def job_application_params
+    params.require(:job_application).permit(:note)
   end
 end
