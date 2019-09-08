5ee059fc5361f87117056b8c3df31087ac312532
Sebastian Lipiszko <lipsikko@gmail.com>
2019-09-08 16:13:14 +1000

    Designs re-seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,23 +10,51 @@ Design.destroy_all
 
 puts "Generating 3 Users..."
 
-count = 100
-
 users = []
 
-3.times { users <<
-  user = User.create!(
+users << user = User.create!(
     first_name: "Simon",
     last_name: "Tyne",
-    email: "user#{count}@user.com",
+    email: "user@user.com",
     password: "123456",
-    job: "Photography Freelancer",
-    bio: "Design lover",
+    job: "Freelance Photographer",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt sunt debitis beatae quos, cum fugit eius quam delectus voluptas laudantium veritatis repellat numquam vero. Unde beatae magni sed nemo consequatur?",
     city: "Berlin",
     photo: open('app/assets/images/avatar-default.png')
-    )
-  count += 1
-}
+  )
+
+users << user = User.create!(
+    first_name: "Hunsi",
+    last_name: "Kumari",
+    email: "user2@user.com",
+    password: "123456",
+    job: "Graphic Design Student",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum corporis officiis, minima iure! Minima commodi omnis voluptas reiciendis quos ratione placeat facilis neque tempore sint libero possimus, eos rem facere.",
+    city: "Dehli",
+    photo: open('app/assets/images/avatar-default.png')
+  )
+
+users << user = User.create!(
+    first_name: "Danielle",
+    last_name: "Viletti",
+    email: "user3@user.com",
+    password: "123456",
+    job: "Illustrator",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla facere nostrum ab aperiam temporibus aliquid reprehenderit natus ipsam officia rem nesciunt est nobis, rerum recusandae eius aspernatur molestias pariatur unde.",
+    city: "Melbourne",
+    photo: open('app/assets/images/avatar-default.png')
+  )
+
+users << user = User.create!(
+    first_name: "Caitlin",
+    last_name: "Owen",
+    email: "user4@user.com",
+    password: "123456",
+    job: "Creative Director",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum nostrum quisquam harum natus modi necessitatibus nobis. Temporibus amet similique id tempore, vitae, cum porro possimus, reiciendis sint odio aliquam quasi.",
+    city: "Melbourne",
+    photo: open('app/assets/images/avatar-default.png')
+  )
 
 puts "Finished!"
 
@@ -36,91 +64,132 @@ designs = []
 
 designs << Design.create!(
   photo: open('app/assets/images/design_test_2.jpeg'),
-  title: "Crisp (Customer Messaging App) Saas website",
-  description: "I had the pleasure to help the guys are Crisp bring their brand to a place worthy of their culture and ambitions.",
-  category: "Web desgin",
+  title: "Traveling Into the Web",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quia quibusdam, similique expedita nisi aliquam nemo alias, architecto perferendis maiores reprehenderit nobis repellendus amet error iusto accusamus rem libero, maxime?",
+  category: "Illustration",
   user: users.sample
 )
 
 designs << Design.create!(
   photo: open('app/assets/images/design_test_4.jpeg'),
-  title: "Windmill town",
-  description: "Is there such a place, the blue sky, the gloden earth, the wind blwoing the grass, everything is so beautiful.",
+  title: "Web Design",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure nam dolores beatae laudantium minus, blanditiis perferendis. Debitis inventore, rem, explicabo, dolore eaque natus consequuntur quis aspernatur dolores nisi, doloremque maxime!",
   category: "Illistration",
   user: users.sample
 )
 
 designs << Design.create!(
-  photo: open('app/assets/images/design_test_3.jpeg'),
-  title: "Fashion Web UI",
-  description: "Fashion webite design.",
-  category: "Web desgin",
-  user: users.sample
-)
-
-designs << Design.create!(
-  photo: open('app/assets/images/design_test_7.jpeg'),
-  title: "Put the Yee in Haw",
-  description: "Monday morning type warm-up inspired by the weekend. Have more weekends that keep you sayin' YEE-FUCKING-HAW!",
+  photo: open('app/assets/images/design_test_16.jpeg'),
+  title: "E-learning Card Design",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis laborum molestiae animi ipsam suscipit quae repudiandae deleniti explicabo neque quasi quam, tempora eveniet, iste veniam fugit maiores. Voluptatibus, quae, facere.",
   category: "Typography",
   user: users.sample
 )
 
 designs << Design.create!(
-  photo: open('app/assets/images/design_test_5.jpeg'),
-  title: "Sunset hike",
-  description: "Personal work / revamp / repost",
-  category: "Illistration",
+  photo: open('app/assets/images/design_test_14.jpeg'),
+  title: "Portfolio Mockup",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam incidunt, quis aliquid accusantium dicta animi. Doloribus qui facere amet sequi itaque ullam dolor dignissimos eligendi accusamus harum. Vero illum, delectus.",
+  category: "Web Design",
   user: users.sample
 )
 
 designs << Design.create!(
-  photo: open('app/assets/images/design_test_1.jpeg'),
-  title: "Food web page mockup",
-  description: "Made this is in my freetime between jobs",
-  category: "Web desgin",
+  photo: open('app/assets/images/design_test_3.jpeg'),
+  title: "The Modern Thinker",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima nesciunt quod aperiam repudiandae ratione repellendus et porro, laborum eum dignissimos pariatur maiores, in aliquam, vero perferendis excepturi. Quo, perspiciatis placeat?",
+  category: "Illustration",
   user: users.sample
 )
 
 designs << Design.create!(
-  photo: open('app/assets/images/design_test_6.jpeg'),
-  title: "Room to Roam Pennant",
-  description: "We love how our Room to Roam Pennant came out! Manufactured",
+  photo: open('app/assets/images/design_test_7.jpeg'),
+  title: "Portugal the Man",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis commodi, at molestiae eum odio. Sit necessitatibus exercitationem nemo earum modi eligendi explicabo doloribus! Laboriosam eaque unde repellendus, eveniet inventore ipsum!",
   category: "Typography",
   user: users.sample
 )
 
 designs << Design.create!(
   photo: open('app/assets/images/design_test_8.jpeg'),
-  title: "Passion fruit",
-  description: "After moving to Taiwan I discovered there were so many various fruits and started to draw some of them. For this project I used my Ipad Pro and my Apple pencil. Yummny!",
+  title: "Moody Canyon",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore voluptates aperiam hic delectus eum sint harum quasi tenetur, fugiat magni eligendi provident assumenda accusamus expedita adipisci libero, quos eveniet debitis.",
+  category: "Photography",
+  user: users.sample
+)
+
+designs << Design.create!(
+  photo: open('app/assets/images/design_test_5.jpeg'),
+  title: "Incredibles Themed Typography Collage",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio perferendis in voluptate blanditiis recusandae aperiam facere magni? Mollitia odit accusamus similique natus tempora, ipsum, deleniti aperiam inventore, dolorum nesciunt repellendus?",
+  category: "Typography",
+  user: users.sample
+)
+
+designs << Design.create!(
+  photo: open('app/assets/images/design_test_1.jpeg'),
+  title: "CampSight",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam cumque libero cum aspernatur iste, modi fugit unde excepturi molestiae, explicabo illo aliquid at, possimus optio perspiciatis provident amet facere officiis!",
+  category: "Illustration",
+  user: users.sample
+)
+
+designs << Design.create!(
+  photo: open('app/assets/images/design_test_6.jpeg'),
+  title: "Thank You Kim & Jun ",
+  description: "Thank you for the time, effort & passion you both put into DesignnHub. N' for believing in me enough to vote for my project. Wish you both all the best with wherever life may take you. N' if you ever need help with something I'm always just a http request away <3",
   category: "Typography",
   user: users.sample
 )
 
 designs << Design.create!(
   photo: open('app/assets/images/design_test_11.jpeg'),
-  title: "Cafe Tropical",
-  description: "Just for fun exploration for this damn funny show.",
-  category: "Illistration",
+  title: "Low Impact",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi magni ex iste perspiciatis molestias ducimus veniam doloremque dolorem suscipit odit eligendi, repudiandae voluptas cupiditate, dolorum aperiam, atque adipisci dignissimos commodi?",
+  category: "Branding",
   user: users.sample
 )
 
 designs << Design.create!(
   photo: open('app/assets/images/design_test_9.jpeg'),
-  title: "VR Tennis Game App",
-  description: "Connect your app to VR glasses, to play with those legendary tennis starts. Select the courts you prefer and choose the different levels",
-  category: "Web desgin",
+  title: "Ocean Love",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis qui nemo id asperiores exercitationem veniam non sapiente, ducimus voluptate ipsum quibusdam molestiae earum. Quas nam, laboriosam iusto perferendis quam. At.",
+  category: "Photography",
   user: users.sample
 )
 
 designs << Design.create!(
   photo: open('app/assets/images/design_test_10.jpeg'),
-  title: "Polar Bear Holding Coca Cola and Flowers",
-  description: "Just for fun",
-  category: "Illistration",
+  title: "Wed Design Components",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat recusandae mollitia rerum atque fugiat odio, nam, voluptatem fuga vitae ab consequuntur eum! Enim itaque amet ad perferendis dicta magni placeat.",
+  category: "Graphic Design",
   user: users.sample
 )
+
+designs << Design.create!(
+  photo: open('app/assets/images/design_test_12.jpeg'),
+  title: "RUAN Bicycles",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores fugiat ratione natus minima, culpa recusandae facere praesentium sapiente inventore adipisci sunt, doloremque saepe molestias excepturi consequatur sit quas voluptas. Animi.",
+  category: "Web Design",
+  user: users.sample
+)
+
+designs << Design.create!(
+  photo: open('app/assets/images/design_test_14.jpeg'),
+  title: "Fushimi Inari Shrine",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid ipsum porro nihil dolore qui dolores assumenda perspiciatis perferendis itaque inventore. Nulla quisquam, rem aut tempora reprehenderit distinctio deleniti architecto est?",
+  category: "Photography",
+  user: users.sample
+)
+
+designs << Design.create!(
+  photo: open('app/assets/images/design_test_17.jpeg'),
+  title: "London Design Biennale",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dignissimos optio fugit qui aperiam. Accusamus velit voluptatum reprehenderit. Cum, asperiores, eaque. Minima neque saepe fugit itaque odio animi dignissimos eligendi.",
+  category: "Typography",
+  user: users.sample
+)
+
 
 puts "Finished!"
 
@@ -128,7 +197,7 @@ puts "Generating Design Tags..."
 
 tags = [
   "Adobe",
-  "Branc",
+  "Brand",
   "Photoshop",
   "Cartoon",
   "Illistration",
@@ -154,7 +223,7 @@ puts "Finished!"
 
 puts "Generating Comments..."
 
-content = [
+comments = [
     "This is my comment",
     "I left my comment here",
     "A comment was posted here",
@@ -163,7 +232,7 @@ content = [
 
 designs.uniq.each do |design|
   5.times { comment = Comment.create!(
-    content: content.sample,
+    content: comments.sample,
     x: rand(0.1..1),
     y: rand(0.1..1),
     design: design,
