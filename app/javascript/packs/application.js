3d83fe011421320464ef5c86a568950c921af92f
jun low <zhijun.low@gmail.com>
2019-09-08 13:01:30 +1000

    import js files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,9 @@
 import "bootstrap";
-import '../components/design_photo_pinner';
 import {markImgWhenClick} from '../components/design_show';
 import { initSelect2 } from '../plugins/init_select2';
 initSelect2();
 
 import '../components/job_sidebar';
 import '../components/job_card';
+import '../components/design_photo_pinner';
+import '../components/design_show';
