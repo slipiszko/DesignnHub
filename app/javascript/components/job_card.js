932adf9a071557dec099ac611814d97896a7322b
jun low <zhijun.low@gmail.com>
2019-09-09 11:49:17 +1000

    Update job_card.js

--- app/javascript/components/job_card.js
+++ app/javascript/components/job_card.js
@@ -1,15 +1,14 @@
 function resizeMasonryItem(item){
-  // Get the grid object, its row-gap, and the size of its implicit rows
+  /* Get the grid object, its row-gap, and the size of its implicit rows */
   const grid = document.querySelectorAll('.masonry')[0];
-
   if( grid ) {
     const rowGap    = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap')),
           rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows')),
-          gridImagesAsContent = item.querySelector('img.masonry-content');
+        gridImagesAsContent = item.querySelector('img.masonry-content');
 
     const rowSpan = Math.ceil((item.querySelector('.masonry-content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
 
-    // Set the spanning as calculated above
+    /* Set the spanning as calculated above (S) */
     item.style.gridRowEnd = 'span '+rowSpan;
     if(gridImagesAsContent) {
       item.querySelector('img.masonry-content').style.height = item.getBoundingClientRect().height + "px";
@@ -21,33 +20,32 @@ function resizeAllMasonryItems(){
   // Get all item class objects in one list
   const allItems = document.querySelectorAll('.masonry-item');
 
-  // Loop through the above list and execute the spanning function to
-  // each masonry item
   if( allItems ) {
-    for(let i = 0; i > allItems.length; i++){
+    for(let i=0;i>allItems.length;i++){
       resizeMasonryItem(allItems[i]);
     }
   }
 }
 
 function waitForImages() {
+  //const grid = document.getElementById("masonry");
   const allItems = document.querySelectorAll('.masonry-item');
   if( allItems ) {
-    for(let i = 0; i < allItems.length; i++){
+    for(let i=0;i<allItems.length;i++){
       imagesLoaded( allItems[i], function(instance) {
         const item = instance.elements[0];
         resizeMasonryItem(item);
-        console.log("Waiting for job posts..");
+        console.log("Waiting for Images");
       } );
     }
   }
 }
 
-// Resize all the grid items on the load and resize events
+/* Resize all the grid items on the load and resize events */
 const masonryEvents = ['load', 'resize'];
 masonryEvents.forEach( function(event) {
   window.addEventListener(event, resizeAllMasonryItems);
 } );
 
-// Do a resize once more when all the images finish loading
+/* Do a resize once more when all the images finish loading */
 waitForImages();
