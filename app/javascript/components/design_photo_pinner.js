440ad6c40cd3a9698bfb3594f2734ce936c4cc23
Kim Nesselhauf <kim.nesselhauf@gmail.com>
2019-09-11 14:32:15 +1000

    pinns almost finish

--- app/javascript/components/design_photo_pinner.js
+++ app/javascript/components/design_photo_pinner.js
@@ -1,13 +1,20 @@
 const designPhotoPinnerComponents = document.querySelectorAll('.js-design-photo-pinner');
 // const baseUrl = `http://localhost:3000/designs/${:id}/edit`;
-const comment = document.querySelector("#js-your-message");
+const commentEl = document.querySelector("#js-your-message");
 // const form = document.querySelector("#js-comment-form");
 const inputX  = document.querySelector(".js-comment-x");
 const inputY  = document.querySelector(".js-comment-y");
+
 const popupCommentFormEl = document.querySelector(".js-popup-comment-form");
+const popupCommentFormInputEl = popupCommentFormEl.querySelector('textarea');
 
 designPhotoPinnerComponents.forEach((photoPinnerComponent) => {
   photoPinnerComponent.addEventListener('click', (e) => {
+    // Only ever move the user pin if the user is actually clicking on the image.
+    if (e.target.nodeName !== 'IMG') {
+      return;
+    }
+
     const photoWidth  = photoPinnerComponent.offsetWidth;
     const photoHeight = photoPinnerComponent.offsetHeight;
     const x = (e.offsetX - 15) / photoWidth;
@@ -19,8 +26,22 @@ designPhotoPinnerComponents.forEach((photoPinnerComponent) => {
     inputY.value = y;
 
     popupCommentFormEl.classList.remove('d-none');
+
+    popupCommentFormInputEl.focus();
   });
 });
 
+popupCommentFormInputEl.addEventListener('keydown', (e) => {
+  // 13 is the enter button.
+  if (e.keyCode === 13) {
+    e.preventDefault(); // Don't create a new line.
+    popupCommentFormInputEl.form.submit(); // Submit the form.
+  }
+});
 
 
+if (popupCommentFormEl) {
+  popupCommentFormEl.addEventListener('click', (e) => {
+    e.stopPropagation();
+  });
+}
