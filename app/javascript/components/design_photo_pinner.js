be4392f379b6bee8987cf12952d150906b144b5c
jun low <zhijun.low@gmail.com>
2019-09-11 18:30:40 +1000

    Added model show on job post

--- app/javascript/components/design_photo_pinner.js
+++ app/javascript/components/design_photo_pinner.js
@@ -6,42 +6,45 @@ const inputX  = document.querySelector(".js-comment-x");
 const inputY  = document.querySelector(".js-comment-y");
 
 const popupCommentFormEl = document.querySelector(".js-popup-comment-form");
-const popupCommentFormInputEl = popupCommentFormEl.querySelector('textarea');
-
-designPhotoPinnerComponents.forEach((photoPinnerComponent) => {
-  photoPinnerComponent.addEventListener('click', (e) => {
-    // Only ever move the user pin if the user is actually clicking on the image.
-    if (e.target.nodeName !== 'IMG') {
-      return;
-    }
-
-    const photoWidth  = photoPinnerComponent.offsetWidth;
-    const photoHeight = photoPinnerComponent.offsetHeight;
-    const x = (e.offsetX - 15) / photoWidth;
-    const y = (e.offsetY - 15) / photoHeight;
-    const pinEl = photoPinnerComponent.querySelector('.js-design-photo-pin');
-    pinEl.style.top = `${(y * 100)}%`;
-    pinEl.style.left = `${(x * 100)}%`;
-    inputX.value = x;
-    inputY.value = y;
-
-    popupCommentFormEl.classList.remove('d-none');
-
-    popupCommentFormInputEl.focus();
-  });
-});
-
-popupCommentFormInputEl.addEventListener('keydown', (e) => {
-  // 13 is the enter button.
-  if (e.keyCode === 13) {
-    e.preventDefault(); // Don't create a new line.
-    popupCommentFormInputEl.form.submit(); // Submit the form.
-  }
-});
-
 
 if (popupCommentFormEl) {
-  popupCommentFormEl.addEventListener('click', (e) => {
-    e.stopPropagation();
+  const popupCommentFormInputEl = popupCommentFormEl.querySelector('textarea');
+
+  designPhotoPinnerComponents.forEach((photoPinnerComponent) => {
+    photoPinnerComponent.addEventListener('click', (e) => {
+      // Only ever move the user pin if the user is actually clicking on the image.
+      if (e.target.nodeName !== 'IMG') {
+        return;
+      }
+
+      const photoWidth  = photoPinnerComponent.offsetWidth;
+      const photoHeight = photoPinnerComponent.offsetHeight;
+      const x = (e.offsetX - 15) / photoWidth;
+      const y = (e.offsetY - 15) / photoHeight;
+      const pinEl = photoPinnerComponent.querySelector('.js-design-photo-pin');
+      pinEl.style.top = `${(y * 100)}%`;
+      pinEl.style.left = `${(x * 100)}%`;
+      inputX.value = x;
+      inputY.value = y;
+
+      popupCommentFormEl.classList.remove('d-none');
+
+      popupCommentFormInputEl.focus();
+    });
   });
+
+  popupCommentFormInputEl.addEventListener('keydown', (e) => {
+    // 13 is the enter button.
+    if (e.keyCode === 13) {
+      e.preventDefault(); // Don't create a new line.
+      popupCommentFormInputEl.form.submit(); // Submit the form.
+    }
+  });
+
+
+  if (popupCommentFormEl) {
+    popupCommentFormEl.addEventListener('click', (e) => {
+      e.stopPropagation();
+    });
+  }
 }
--- app/javascript/components/job_card.js
+++ app/javascript/components/job_card.js
@@ -3,11 +3,21 @@ import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 
 const elem = document.querySelector('.masonry');
-const msnry = new Masonry( elem, {
-  // options
-  itemSelector: '.masonry-item',
-  gutter: 20
+if (elem) {
+  const msnry = new Masonry( elem, {
+    // options
+    itemSelector: '.masonry-item',
+    gutter: 20,
+  });
+
+  AOS.init();
+}
+
+const applyButtonEl = document.querySelector('.js-post-apply-btn');
+const postInitialContentEl = document.querySelector('.js-post-initial-content');
+const postApplyContentEl = document.querySelector('.js-post-apply-content');
+applyButtonEl.addEventListener('click', (e) => {
+  postInitialContentEl.classList.add('d-none');
+  postApplyContentEl.classList.remove('d-none');
+  e.preventDefault();
 });
-
-
-AOS.init();
