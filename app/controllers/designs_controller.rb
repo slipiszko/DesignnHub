ecf60fc4ec35c63f70edcd668bca5d2797c2d1f7
Sebastian Lipiszko <lipsikko@gmail.com>
2019-09-12 17:35:45 +1000

    Designs associated to design_tags through join table

--- app/controllers/designs_controller.rb
+++ app/controllers/designs_controller.rb
@@ -26,6 +26,7 @@ class DesignsController < ApplicationController
   def new
     @design = Design.new
     @user = current_user
+    @design.design_tags.build(name: 'submitter').build_entity
   end
 
   def create
@@ -57,6 +58,6 @@ class DesignsController < ApplicationController
   end
 
   def design_params
-    params.require(:design).permit(:photo, :title, :description, :category, design_tag_ids: [])
+    params.require(:design).permit(:photo, :title, :description, :category, design_tags_attributes: [:id, :name])
   end
 end
