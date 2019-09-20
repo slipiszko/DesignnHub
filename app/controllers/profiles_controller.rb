39eaa540e159dc16848b8696628aba6d946918cd
Sebastian Lipiszko <lipsikko@gmail.com>
2019-09-20 12:09:23 +1000

    Removed user authentication on profile show

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show]
+
   def show
     @current_user = current_user
     @user = User.find(params[:id])
