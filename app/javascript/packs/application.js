aa79b2906def594ea6de345fdeea10f0552ead98
Sebastian Lipiszko <lipsikko@gmail.com>
2019-09-20 15:54:11 +1000

    Created job_post_card.js to make entire card a clickable link

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,7 @@ import "../plugins/flatpickr"
 
 import '../components/job_sidebar';
 import '../components/job_card';
+// import '../components/job_post_card';
 import '../components/design_photo_pinner';
 import '../components/design_show';
 
