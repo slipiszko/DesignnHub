1578b6653e18e1dd24ec41b374474f4cb9e5fa65
Sebastian Lipiszko <lipsikko@gmail.com>
2019-09-30 07:56:28 +1000

    QuestionTag cards view template created

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,7 @@ class QuestionsController < ApplicationController
 
   def index
     @questions = policy_scope(Question).order(created_at: :desc)
+    @question_tags = QuestionTag.all
   end
 
   def show
