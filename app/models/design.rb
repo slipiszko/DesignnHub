75c8ed32f7f310fa6cd383961a36fa02fd14125c
Sebastian Lipiszko <lipsikko@gmail.com>
2019-10-13 18:19:22 +1100

    Associations created

--- app/models/design.rb
+++ app/models/design.rb
@@ -3,6 +3,7 @@ class Design < ApplicationRecord
 
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :design_design_tags
   has_many :design_tags, through: :design_design_tags
 
   accepts_nested_attributes_for :design_tags
