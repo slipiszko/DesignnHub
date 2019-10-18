e99c2869a0d218782033863afbc4f507c2494c50
Sebastian Lipiszko <lipsikko@gmail.com>
2019-10-18 15:52:34 +1100

    Critique create method created.

--- app/controllers/critiques_controller.rb
+++ app/controllers/critiques_controller.rb
@@ -1,2 +1,29 @@
 class CritiquesController < ApplicationController
+  before_action :set_critique_user,    only: [:new, :create, :edit, :update]
+  before_action :set_critique_portfolio,  only: [:new, :create, :edit, :update]
+
+  def new
+    @critique = Critique.new
+  end
+
+  def create
+    @critique = Critique.new(critique_params)
+    @critique.user = @user
+    @critique.portfolio = @portfolio
+    authorize @critique
+  end
+
+  private
+
+  def critique_params
+    params.require(:critique).permit(:content)
+  end
+
+  def set_critique_user
+    @user = current_user
+  end
+
+  def set_critique_portfolio
+    @portfolio = Portfolio.find(params[:design_id])
+  end
 end
