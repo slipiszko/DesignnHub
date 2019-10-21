b07b48b7329c29684df67fa6f09fbfaa2ccd4919
Sebastian Lipiszko <lipsikko@gmail.com>
2019-10-22 09:25:31 +1100

    Pundit policy created.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,13 @@
 class ArticlesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content, :photo)
+  end
 end
