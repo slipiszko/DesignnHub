9bed635bd644cd9bc6af342eecadcfe8f25f3d98
Sebastian Lipiszko <lipsikko@gmail.com>
2019-10-22 09:30:28 +1100

    Article CRUD methods created.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,45 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+
   def index
+    @articles = policy_scope(Article).order(created_at: :desc)
   end
 
   def show
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to root_path
   end
 
   private
 
   def article_params
     params.require(:article).permit(:title, :content, :photo)
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
   end
 end
