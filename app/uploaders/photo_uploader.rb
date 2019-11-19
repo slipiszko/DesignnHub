05c64851e2d819fe30485c6be1e23b5717d1638a
Sebastian Lipiszko <lipsikko@gmail.com>
2019-11-19 16:59:47 +1100

    Styled all devise forms.

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,3 +1,23 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+  # include CarrierWave::MiniMagick
+
+  # process convert: 'jpeg'
+  # process resize_to_limit: [400, 280]
+
+  version :full do
+    eager
+    resize_to_fit(800, 600)
+    cloudinary_transformation quality: 100
+  end
+
+  version :standard do
+    eager
+    resize_to_fit(400, 300)
+    cloudinary_transformation quality: 100
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg png)
+  end
 end
